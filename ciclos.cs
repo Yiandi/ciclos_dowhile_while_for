ce7258252961ae5b48afbf7ab80f853d84721d8d
Yiandi <yiandipdlc05@gmail.com>
2026-04-10 16:35:30 -0400

    Add loop examples in ciclos.cs
    Added examples of for, do-while, and while loops in C#.

---
+++ ciclos.cs
@@ -0,0 +1,43 @@
+
+for (int i = 10; i >= 1; i--)
+{
+    Console.WriteLine(i);
+}
+
+-----------------------------------
+  
+for (inicialización; condición; incremento)
+{
+    // código que se rep
+}
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+---------------------------------------
+int i = 1;
+
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i <= 5);
+
+
+-------------------------------------
+
+
+  
+int i = 1;
+
+while (i <= 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+
+
